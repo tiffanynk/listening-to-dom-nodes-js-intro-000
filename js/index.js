1a37f22abc86c05d040ced7a36d79d7477037db5
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 18:40:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
