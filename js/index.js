57628da28165b449029b826ca8a7488b45d88a5b
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 18:49:46 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,4 +22,14 @@ function bubble(e) {
 
 for ( let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
+};
+
+divs = document.querySelectorAll('div');
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', capture, true);
 }
