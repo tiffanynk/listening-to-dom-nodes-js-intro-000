5de3b40bd11953d44f0c28d575bd429e5ebaa3e3
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 18:47:06 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,3 +13,13 @@ input.addEventListener('keydown', function(e) {
     console.log(e.key);
   }
 });
+
+let divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + 'bubbled');
+}
+
+for ( let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
